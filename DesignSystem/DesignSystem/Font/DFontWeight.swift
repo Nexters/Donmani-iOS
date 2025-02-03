e24a6583663b5a303facca57629f42201cd76123
moonjs0113 <littlemoom@naver.com>
2025-02-03 23:53:16 +0900

    Add DFont in DesignSystem

---
+++ DesignSystem/DesignSystem/Font/DFontWeight.swift
@@ -0,0 +1,14 @@
+//
+//  DFontWeight.swift
+//  DesignSystem
+//
+//  Created by 문종식 on 2/3/25.
+//
+
+
+public enum DFontWeight: String {
+    case bold = "Bold"
+    case semibold = "SemiBold"
+    case medium = "Medium"
+    case regular = "Regular"
+}
