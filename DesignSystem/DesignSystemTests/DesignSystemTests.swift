0efb81ea8750dae77ca4975b4440b2d9337de301
moonjs0113 <littlemoom@naver.com>
2025-02-03 21:56:03 +0900

    Add Workspace

---
+++ DesignSystem/DesignSystemTests/DesignSystemTests.swift
@@ -0,0 +1,17 @@
+//
+//  DesignSystemTests.swift
+//  DesignSystemTests
+//
+//  Created by 문종식 on 2/3/25.
+//
+
+import Testing
+@testable import DesignSystem
+
+struct DesignSystemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
