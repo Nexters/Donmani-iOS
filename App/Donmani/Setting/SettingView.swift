2054aa66eaf8da519f6dd4cea42e131156ca7fc6
moonjs0113 <littlemoom@naver.com>
2025-02-04 23:45:58 +0900

    Migrate DImage

--- App/Donmani/Setting/SettingView.swift
+++ App/Donmani/Setting/SettingView.swift
@@ -6,57 +6,64 @@
 //
 
 import SwiftUI
+import DesignSystem
 
 struct SettingView: View {
     @Environment(\.dismiss) private var dismiss
     let width = UIScreen.main.bounds.width
-    // TODO: Store 
     let defaultPadding: CGFloat = 20
     
+    // TODO: - Add Store
+    
     var body: some View {
-        VStack(alignment: .center, spacing: defaultPadding) {
-            ZStack {
-                HStack {
-                    Button {
-                        dismiss()
-                    } label: {
-                        ZStack {
-                            Rectangle()
-                                .fill(.clear)
-                                .frame(width: 24, height: 24)
-                            Image(systemName: "chevron.left")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(height: 14)
-                                .foregroundStyle(.black)
+        ZStack {
+            BackgroundView()
+            VStack(alignment: .center, spacing: defaultPadding) {
+                ZStack {
+                    HStack {
+                        Button {
+                            dismiss()
+                        } label: {
+                            ZStack {
+                                Rectangle()
+                                    .fill(.clear)
+                                    .frame(width: 24, height: 24)
+                                DImage(.leftArrow).image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(height: 14)
+                                    .foregroundStyle(.black)
+                            }
                         }
+                        Spacer()
                     }
-                    Spacer()
+                    .padding(.horizontal, defaultPadding)
+                    Text("설정")
+                        .font(.system(size: 16, weight: .semibold))
+                        .foregroundStyle(.white)
                 }
-                .padding(.horizontal, defaultPadding)
-                Text("설정")
-                    .font(.system(size: 16, weight: .semibold))
-            }
-            .padding(.vertical, 14)
-            
-            VStack(alignment: .center, spacing: 12) {
-                Image("TempImage")
-                Text("노래하는 농담곰")
-                    .font(.b1, .semibold)
-            }
-            .padding(defaultPadding)
-            .padding(.bottom, defaultPadding)
-            
-            VStack(alignment: .leading, spacing: 0) {
-                MenuButton(title: "개인정보 처리방침") {
-                    
-                }
+                .padding(.vertical, 14)
                 
-                MenuButton(title: "피드백") {
-                    
+                VStack(alignment: .center, spacing: 12) {
+                    DImage(.tempImage).image
+                    Text("노래하는 농담곰")
+                        .font(.b1, .semibold)
+                        .foregroundStyle(.white)
                 }
+                .padding(defaultPadding)
+                .padding(.bottom, defaultPadding)
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    MenuButton(title: "개인정보 처리방침") {
+                        
+                    }
+                    
+                    MenuButton(title: "피드백") {
+                        
+                    }
+                }
+                Spacer()
             }
-            Spacer()
         }
         .navigationBarBackButtonHidden()
     }
@@ -70,7 +77,7 @@ struct SettingView: View {
         } label: {
             Text(title)
                 .font(.b1, .bold)
-                .foregroundStyle(.black)
+                .foregroundStyle(.white)
                 .frame(width: width - defaultPadding * 2, alignment: .leading)
                 .padding(.horizontal, defaultPadding)
                 .padding(.vertical, 16)
