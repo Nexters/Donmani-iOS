c470e753c6636a340ecad2c4e9b7b475abadb5df
moonjs0113 <littlemoom@naver.com>
2025-02-04 23:11:56 +0900

    Add DImage

---
+++ DesignSystem/DesignSystem/Image/DImageAsset.swift
@@ -0,0 +1,20 @@
+//
+//  DImageAsset.swift
+//  DesignSystem
+//
+//  Created by 문종식 on 2/4/25.
+//
+
+import SwiftUI
+
+public enum DImageAsset: String {
+    case backgroundDot = "BackgroundDot"
+    case backgroundStar = "BackgroundStar"
+    case add = "add"
+    case edit = "Edit"
+    case leftArrow = "LeftArrow"
+    case rightArrow = "RightArrow"
+    case calendar = "Calendar"
+    case setting = "Setting"
+    case tempImage = "TempImage"
+}
